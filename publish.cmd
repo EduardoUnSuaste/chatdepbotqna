224a87c43c366b603034a365714bf30a55f4d901
EduardoUnSuaste <50590469+EduardoUnSuaste@users.noreply.github.com>
2020-06-20 23:37:25 -0500

    Actualizacion 1
    ChatDepBot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatdepbotqna-bot-Web-Deploy.pubxml -p:Password=mleEKTRlkf90ymcYQgTlRn3EZBatfueaRsdM3PW59dweL4xcg8clMqCihcZE
+
